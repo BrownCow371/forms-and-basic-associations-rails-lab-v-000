c123add06933fe6ae926f4698244fefac347fa0e
Jamie Brown <jmbrowncow@gmail.com>
2018-08-26 20:48:20 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -21,7 +21,7 @@ class Song < ActiveRecord::Base
 
   def notes_values=(values)
     values.each do |value|
-      self.notes << value if value
+      self.notes_build(content: value) if value
     end
   end
 

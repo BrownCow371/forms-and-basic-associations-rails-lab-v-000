0829564f7ba9c07d59606dbf6e85d205dd3daff3
Jamie Brown <jmbrowncow@gmail.com>
2018-08-26 20:26:33 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,5 +6,9 @@ class Song < ActiveRecord::Base
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
   end
-end
 
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+end

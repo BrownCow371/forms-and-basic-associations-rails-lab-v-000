f579694f529a5be0c53315232a9a13c1c17e385e
Jamie Brown <jmbrowncow@gmail.com>
2018-08-26 21:01:28 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,12 +11,12 @@ class Song < ActiveRecord::Base
     self.artist ? self.artist.name : nil
   end
 
-  def genre_name=(name)
+  def genre_id=(name)
     self.genre = Genre.find_or_create_by(name: name)
   end
 
-  def genre_name
-    self.genre ? self.genre.name : nil
+  def genre_id
+    self.genre ? self.genre.id : nil
   end
 
   def notes_values=(values)
